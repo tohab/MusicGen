b2c96ef76ebc4d9cf34b560e33156bfaca3c3243
tohab <prasadr@hsln_17568.local>
2014-10-31 12:26:43 +0000

    Made things neater

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import javax.swing.Timer;
 
 
 public class Main {
-	public static void main(String[] args) throws MidiUnavailableException, InterruptedException {
-		Synthesizer syn = MidiSystem.getSynthesizer();
-		syn.open();   
-		final MidiChannel[] mc = syn.getChannels();
-		Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
-		syn.loadInstrument(instr[91]);
-		for (int i = 0; i < 25; i++) {
+	static MidiChannel[] mc = null;
+	
+	public static void main(String[] args) throws InterruptedException, MidiUnavailableException {
+		setup();
+		play(60,500);
+		/*for (int i = 0; i < 25; i++) {
 			int ran = (int)(Math.random() * 24) + 48;
 			int ran2 = (int)(Math.random() * 10) + 1;
 			mc[5].noteOn(ran,60*ran2);
@@ -21,6 +20,17 @@ public class Main {
 			mc[5].noteOn(48 + (int)(Math.random()*3)*12 + minor[(int)(Math.random()*8)],100);
 			new Thread().sleep(200);
 		}
-		mc[5].noteOff(0);
+		mc[5].noteOff(0);*/
+		
+	}
+	public static void setup() throws MidiUnavailableException {
+		Synthesizer syn = MidiSystem.getSynthesizer();
+		syn.open();   
+		mc = syn.getChannels();
+		Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
+		syn.loadInstrument(instr[91]);
+	}
+	public static void play(int note, int velocity) {
+		mc[5].noteOn(note,velocity);
 	}
 }
