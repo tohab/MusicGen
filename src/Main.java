e91d85d0b52751c11b626c2e1765044bbf9cbb21
tohab <prasadr@hsln_17568.local>
2014-10-31 11:32:36 +0000

    Test

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,18 @@ public class Main {
 		final MidiChannel[] mc = syn.getChannels();
 		Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
 		syn.loadInstrument(instr[91]);
-		/*for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 25; i++) {
 			int ran = (int)(Math.random() * 24) + 48;
 			int ran2 = (int)(Math.random() * 10) + 1;
 			mc[5].noteOn(ran,60*ran2);
 			new Thread().sleep(100*ran2);
-		}*/
+		}
 		int[] major = {0,2,4,5,7,9,11,12};
-		for (int i = 0; i < 100; i++) {
-			mc[5].noteOn(48 + (int)(Math.random()*3)*12 + major[(int)(Math.random()*8)],100);
+		int[] minor = {0,2,3,5,7,8,11,12};
+		for (int i = 0; i < 150; i++) {
+			mc[5].noteOn(48 + (int)(Math.random()*3)*12 + minor[(int)(Math.random()*8)],100);
 			new Thread().sleep(200);
 		}
+		mc[5].noteOff(0);
 	}
 }
