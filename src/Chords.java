1eddb1f4d8afb0b3861eb744c0f8fd4361b1e451
Tristan Burke <tristan_burke@asl.org>
2014-11-04 10:17:06 +0000

    First Commit

---
+++ src/Chords.java
@@ -0,0 +1,68 @@
+import javax.sound.midi.*;
+import javax.swing.Timer;
+
+
+public class Chords {
+   static MidiChannel[] mc = null;
+ public static void main(String[] args) throws MidiUnavailableException, InterruptedException {
+  setup();
+  major(0,1);
+  firstinversionmaj(0,1);
+  major(12,4);
+ }
+public static void setup() throws MidiUnavailableException {
+  Synthesizer syn = MidiSystem.getSynthesizer();
+  syn.open();   
+  mc = syn.getChannels();
+  Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
+  syn.loadInstrument(instr[91]);
+ }
+public static void major(int key,int rep) throws InterruptedException, MidiUnavailableException{
+    for (int i=0; i<rep; i++){   
+      mc[5].noteOn(60+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(60+key,0);
+      mc[5].noteOn(67+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(67+key,0);
+      mc[5].noteOn(64+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(64+key,0);
+      mc[5].noteOn(67+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(67+key,0);
+    }
+  }
+public static void minor(int key) throws InterruptedException, MidiUnavailableException{
+    for (int i=0; i<4; i++){   
+      mc[5].noteOn(60+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(60+key,0);
+      mc[5].noteOn(67+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(67+key,0);
+      mc[5].noteOn(63+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(63+key,0);
+      mc[5].noteOn(67+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(67+key,0);
+    }
+  }
+public static void firstinversionmaj(int key,int rep) throws InterruptedException, MidiUnavailableException{
+    for (int i=0; i<rep; i++){   
+      mc[5].noteOn(64+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(64+key,0);
+      mc[5].noteOn(72+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(72+key,0);
+      mc[5].noteOn(67+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(67+key,0);
+      mc[5].noteOn(72+key,100);
+      new Thread().sleep(200);
+      mc[5].noteOff(72+key,0);
+    }
+  }
+}
