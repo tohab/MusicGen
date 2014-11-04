7fec4c91b2f88ff12d4b675eb445936aa59ae395
Tristan Burke <tristan_burke@asl.org>
2014-11-04 10:44:37 +0000

    Start Method

--- src/Chords.java
+++ src/Chords.java
@@ -30,8 +30,8 @@ public static void major(int key,int rep) throws InterruptedException, MidiUnava
       mc[5].noteOff(67+key,0);
     }
   }
-public static void minor(int key) throws InterruptedException, MidiUnavailableException{
-    for (int i=0; i<4; i++){   
+public static void minor(int key, int rep) throws InterruptedException, MidiUnavailableException{
+    for (int i=0; i<rep; i++){   
       mc[5].noteOn(60+key,100);
       new Thread().sleep(200);
       mc[5].noteOff(60+key,0);
@@ -61,5 +61,23 @@ public static void firstinversionmaj(int key,int rep) throws InterruptedExceptio
       new Thread().sleep(200);
       mc[5].noteOff(72+key,0);
     }
-  }
+  }public static void start(int key, int tone) throws InterruptedException, MidiUnavailableException{
+    if (tone == 1){
+        while(true){
+            major(key,2);
+            if (Math.random()<0.5){
+                major(key+7,2);
+                if (Math.random()<0.3){
+                    minor(key+9,2);
+                    minor(key+2,1);
+                    major(key+7,1);
+                }
+            }else{
+                major(key+5,2);
+                if (Math.random()>0.5)
+                    major(key+7,2);
+                }
+        }
+    }
+}
 }
