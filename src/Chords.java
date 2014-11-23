2def31b58e9dd3ab3b67869d0fdb60b8f31634ff
tohab <rohan_prasad@asl.org>
2014-11-23 13:53:16 +0000

    Worked on GUI

--- src/Chords.java
+++ src/Chords.java
@@ -1,14 +1,14 @@
 import javax.sound.midi.*;
 import javax.swing.Timer;
 
-
 public class Chords {
-    static MidiChannel[] mc = null;
-    public Chords() {
-    	
-    	try {
-    		setup();
-			start(0,1,100);
+	static MidiChannel[] mc = null;
+
+	public Chords() {
+
+		try {
+			setup();
+			start(0, 1, 350);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -16,160 +16,171 @@ public class Chords {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
-    
-public static void setup() throws MidiUnavailableException {
-  Synthesizer syn = MidiSystem.getSynthesizer();
-  syn.open();   
-  mc = syn.getChannels();
-  Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
-  syn.loadInstrument(instr[91]);
- }
-public static void major(int key,int rep, int tempo) throws InterruptedException, MidiUnavailableException{
-    for (int i=0; i<rep; i++){   
-      mc[5].noteOn(60+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(60+key,0);
-      mc[5].noteOn(67+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(67+key,0);
-      mc[5].noteOn(64+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(64+key,0);
-      mc[5].noteOn(67+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(67+key,0);
-    }
-  }
-public static void minor(int key,int rep, int tempo) throws InterruptedException, MidiUnavailableException{
-    for (int i=0; i<rep; i++){   
-      mc[5].noteOn(60+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(60+key,0);
-      mc[5].noteOn(67+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(67+key,0);
-      mc[5].noteOn(63+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(63+key,0);
-      mc[5].noteOn(67+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(67+key,0);
-    }
-  }
-public static void firstinversionmaj(int key,int rep, int tempo) throws InterruptedException, MidiUnavailableException{
-    for (int i=0; i<rep; i++){   
-      mc[5].noteOn(64+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(64+key,0);
-      mc[5].noteOn(72+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(72+key,0);
-      mc[5].noteOn(67+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(67+key,0);
-      mc[5].noteOn(72+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(72+key,0);
-    }
-  }
-public static void secondinversionmaj(int key,int rep, int tempo) throws InterruptedException, MidiUnavailableException{
-    for (int i=0; i<rep; i++){   
-      mc[5].noteOn(67+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(67+key,0);
-      mc[5].noteOn(76+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(76+key,0);
-      mc[5].noteOn(72+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(72+key,0);
-      mc[5].noteOn(76+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(76+key,0);
-    }
-  }
-public static void secondinversionmin(int key,int rep, int tempo) throws InterruptedException, MidiUnavailableException{
-    for (int i=0; i<rep; i++){   
-      mc[5].noteOn(67+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(67+key,0);
-      mc[5].noteOn(75+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(75+key,0);
-      mc[5].noteOn(72+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(72+key,0);
-      mc[5].noteOn(75+key,100);
-      new Thread().sleep(tempo);
-      mc[5].noteOff(75+key,0);
-    }
-  }
-public static void start(int key, int tone, int tempo) throws InterruptedException, MidiUnavailableException{
-    if (tone == 1){
-        while(true){
-            if (Math.random()<.9){
-                major(key,2,tempo);
-            }else{
-                firstinversionmaj(key-5,2,tempo);
-            }
-            if (Math.random()<0.25){
-                major(key+4,2,tempo);
-                secondinversionmin(key-3,2,tempo);
-                major(key+2,2,tempo);
-                secondinversionmaj(key-5,2,tempo);
-                firstinversionmaj(key-5,2,tempo);
-            }
-            if (Math.random()<0.5){
-                if (Math.random()<0.4){
-                    if (Math.random()<0.55){
-                        firstinversionmaj(key-5,2,tempo);
-                    }else{
-                        major(key+2,2,tempo);
-                        secondinversionmaj(key-5,2,tempo);
-                        firstinversionmaj(key-5,2,tempo);
-                    }
-                }else{
-                major(key+7,2,tempo);
-                if (Math.random()<0.4){
-                    minor(key+9,2,tempo);
-                    minor(key+2,1,tempo);
-                    major(key+7,1,tempo);
-                }
-                }
-            }else{
-                if (Math.random()<0.4){
-                    secondinversionmaj(key-7,2,tempo);
-                    if (Math.random()<0.5)
-                    firstinversionmaj(key-5,2,tempo);
-                }
-                else{
-             major(key+5,2,tempo);
-              if (Math.random()<0.5)
-                 major(key+7,2,tempo);
-                }
-           }
-        }    
-      }else{
-         while(true){
-            minor(key,2,tempo);
-            if (Math.random()<0.5){
-                if (Math.random()<0.4){
-                firstinversionmaj(key-5,2,tempo);
-                }else{
-                major(key+7,2,tempo);
-                if (Math.random()<0.45){
-                    major(key+9,2,tempo);
-                    major(key+2,1,tempo);
-                    major(key+7,1,tempo);
-                }
-                }
-            }else{
-                minor(key+5,2,tempo);
-                if (Math.random()>0.5)
-                    major(key+7,2,tempo);
-                }
-        }      
-    }
-}
+	}
+
+	public static void setup() throws MidiUnavailableException {
+		Synthesizer syn = MidiSystem.getSynthesizer();
+		syn.open();
+		mc = syn.getChannels();
+		Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
+		syn.loadInstrument(instr[91]);
+	}
+
+	public static void major(int key, int rep, int tempo)
+			throws InterruptedException, MidiUnavailableException {
+		for (int i = 0; i < rep; i++) {
+			mc[5].noteOn(60 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(60 + key, 0);
+			mc[5].noteOn(67 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(67 + key, 0);
+			mc[5].noteOn(64 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(64 + key, 0);
+			mc[5].noteOn(67 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(67 + key, 0);
+		}
+	}
+
+	public static void minor(int key, int rep, int tempo)
+			throws InterruptedException, MidiUnavailableException {
+		for (int i = 0; i < rep; i++) {
+			mc[5].noteOn(60 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(60 + key, 0);
+			mc[5].noteOn(67 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(67 + key, 0);
+			mc[5].noteOn(63 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(63 + key, 0);
+			mc[5].noteOn(67 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(67 + key, 0);
+		}
+	}
+
+	public static void firstinversionmaj(int key, int rep, int tempo)
+			throws InterruptedException, MidiUnavailableException {
+		for (int i = 0; i < rep; i++) {
+			mc[5].noteOn(64 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(64 + key, 0);
+			mc[5].noteOn(72 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(72 + key, 0);
+			mc[5].noteOn(67 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(67 + key, 0);
+			mc[5].noteOn(72 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(72 + key, 0);
+		}
+	}
+
+	public static void secondinversionmaj(int key, int rep, int tempo)
+			throws InterruptedException, MidiUnavailableException {
+		for (int i = 0; i < rep; i++) {
+			mc[5].noteOn(67 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(67 + key, 0);
+			mc[5].noteOn(76 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(76 + key, 0);
+			mc[5].noteOn(72 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(72 + key, 0);
+			mc[5].noteOn(76 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(76 + key, 0);
+		}
+	}
+
+	public static void secondinversionmin(int key, int rep, int tempo)
+			throws InterruptedException, MidiUnavailableException {
+		for (int i = 0; i < rep; i++) {
+			mc[5].noteOn(67 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(67 + key, 0);
+			mc[5].noteOn(75 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(75 + key, 0);
+			mc[5].noteOn(72 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(72 + key, 0);
+			mc[5].noteOn(75 + key, 100);
+			new Thread().sleep(tempo);
+			mc[5].noteOff(75 + key, 0);
+		}
+	}
+
+	public static void start(int key, int tone, int tempo)
+			throws InterruptedException, MidiUnavailableException {
+		if (tone == 1) {
+			while (true) {
+				if (Math.random() < .9) {
+					major(key, 2, tempo);
+				} else {
+					firstinversionmaj(key - 5, 2, tempo);
+				}
+				if (Math.random() < 0.25) {
+					major(key + 4, 2, tempo);
+					secondinversionmin(key - 3, 2, tempo);
+					major(key + 2, 2, tempo);
+					secondinversionmaj(key - 5, 2, tempo);
+					firstinversionmaj(key - 5, 2, tempo);
+				}
+				if (Math.random() < 0.5) {
+					if (Math.random() < 0.4) {
+						if (Math.random() < 0.55) {
+							firstinversionmaj(key - 5, 2, tempo);
+						} else {
+							major(key + 2, 2, tempo);
+							secondinversionmaj(key - 5, 2, tempo);
+							firstinversionmaj(key - 5, 2, tempo);
+						}
+					} else {
+						major(key + 7, 2, tempo);
+						if (Math.random() < 0.4) {
+							minor(key + 9, 2, tempo);
+							minor(key + 2, 1, tempo);
+							major(key + 7, 1, tempo);
+						}
+					}
+				} else {
+					if (Math.random() < 0.4) {
+						secondinversionmaj(key - 7, 2, tempo);
+						if (Math.random() < 0.5)
+							firstinversionmaj(key - 5, 2, tempo);
+					} else {
+						major(key + 5, 2, tempo);
+						if (Math.random() < 0.5)
+							major(key + 7, 2, tempo);
+					}
+				}
+			}
+		} else {
+			while (true) {
+				minor(key, 2, tempo);
+				if (Math.random() < 0.5) {
+					if (Math.random() < 0.4) {
+						firstinversionmaj(key - 5, 2, tempo);
+					} else {
+						major(key + 7, 2, tempo);
+						if (Math.random() < 0.45) {
+							major(key + 9, 2, tempo);
+							major(key + 2, 1, tempo);
+							major(key + 7, 1, tempo);
+						}
+					}
+				} else {
+					minor(key + 5, 2, tempo);
+					if (Math.random() > 0.5)
+						major(key + 7, 2, tempo);
+				}
+			}
+		}
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,60 @@
 import javax.sound.midi.*;
 import javax.swing.Timer;
 
-/*
-public class Main {
-	static MidiChannel[] mc = null;
-	
-	public static void main(String[] args) throws InterruptedException, MidiUnavailableException {
-		setup();
-		play(60,500);
-		/*for (int i = 0; i < 25; i++) {
-			int ran = (int)(Math.random() * 24) + 48;
-			int ran2 = (int)(Math.random() * 10) + 1;
-			mc[5].noteOn(ran,60*ran2);
-			new Thread().sleep(100*ran2);
-		}
-		int[] major = {0,2,4,5,7,9,11,12};
-		int[] minor = {0,2,3,5,7,8,11,12};
-		for (int i = 0; i < 150; i++) {
-			mc[5].noteOn(48 + (int)(Math.random()*3)*12 + minor[(int)(Math.random()*8)],100);
-			new Thread().sleep(200);
-		}
-		mc[5].noteOff(0);*/
-		
-	/*}
-	public static void setup() throws MidiUnavailableException {
-		Synthesizer syn = MidiSystem.getSynthesizer();
-		syn.open();   
-		mc = syn.getChannels();
-		Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
-		syn.loadInstrument(instr[91]);
-	}
-	public static void play(int note, int velocity) {
-		mc[5].noteOn(note,velocity);
-}
-}*/
-
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
- 
+import javafx.stage.WindowEvent;
+
 public class Main extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-                new Chords();
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Music Generator");
+		Button btn = new Button();
+		btn.setText("Say 'Hello World'");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Start da' music!");
+				new Chords();
+				primaryStage.close();
+				
+			}
+		});
+
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		grid.add(btn,5,4);
+		
+		Text scenetitle = new Text("Welcome");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(scenetitle, 7, 7, 3, 3);
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+	          public void handle(WindowEvent we) {
+	              System.out.println("Stage is closing");
+	          }
+	      });        
+	        primaryStage.close();
+		primaryStage.setScene(new Scene(grid, 600, 400));
+		primaryStage.show();
+	}
 }
