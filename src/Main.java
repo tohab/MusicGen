d26cbfe54a13a5fd587d5c62ed19fa84d4e90760
tohab <rohan_prasad@asl.org>
2014-11-29 14:35:49 +0000

    Worked on GUI

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ import javafx.event.*;
 import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
@@ -15,6 +18,12 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class Main extends Application {
+
+	GridPane grid = new GridPane();
+	int tempo = 124;
+	int tone = 1;
+	Slider slider = new Slider();
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -22,39 +31,112 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Music Generator");
+		
+		grid.setAlignment(Pos.TOP_LEFT);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+
+		setupButton(primaryStage);
+		setupWelcome(primaryStage);
+		setupSlider(primaryStage);
+		setupKey(primaryStage);
+		
+		// Set red button to close window.
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			public void handle(WindowEvent we) {
+				System.out.println("Stage is closing");
+			}
+		});
+		
+		// Add grid to the scene.
+		primaryStage.close();
+		primaryStage.setScene(new Scene(grid, 600, 400));
+		primaryStage.show();
+	}
+
+	private void setupKey(Stage primaryStage) {
+		final ToggleGroup group = new ToggleGroup();
+
+		RadioButton rb1 = new RadioButton("Major");
+		rb1.setToggleGroup(group);
+		rb1.setSelected(true);
+		rb1.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				tone = 1;
+				System.out.println("major??");
+			}
+		});
+
+		RadioButton rb2 = new RadioButton("Minor");
+		rb2.setToggleGroup(group);
+		
+		rb2.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				tone = 0;
+			}
+		});
+		
+		grid.add(rb1, 15, 17);
+		grid.add(rb2, 15, 18);		
+	}
+
+	private void setupSlider(Stage primaryStage) {
+		slider.setMin(40);
+		slider.setMax(208);
+		slider.setValue(124);
+		slider.setShowTickLabels(true);
+		slider.setShowTickMarks(true);
+		slider.setMajorTickUnit(40);
+		slider.setMinorTickCount(5);
+		slider.setBlockIncrement(5);
+		slider.setScaleX(2);
+		slider.setScaleY(2);
+		
+		grid.add(slider, 15, 25);
+	}
+
+	private void setupWelcome(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		Text scenetitle = new Text("Welcome");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(scenetitle, 15, 5);
+	}
+
+	private void setupButton(Stage primaryStage) {
+		// Create button and effect of pressing it.
 		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
+		btn.setText("Start!");
 		btn.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
 				System.out.println("Start da' music!");
-				new Chords();
-				primaryStage.close();
 				
+				try {
+					new Chords().start(3, tone, tempoConversion(slider.getValue())) ;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (MidiUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				primaryStage.close();
+
 			}
 		});
 
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		
-		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(25, 25, 25, 25));
-		grid.add(btn,5,4);
-		
-		Text scenetitle = new Text("Welcome");
-		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		grid.add(scenetitle, 7, 7, 3, 3);
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-	          public void handle(WindowEvent we) {
-	              System.out.println("Stage is closing");
-	          }
-	      });        
-	        primaryStage.close();
-		primaryStage.setScene(new Scene(grid, 600, 400));
-		primaryStage.show();
+		//Add button to the world.	
+		grid.add(btn, 5, 5);
 	}
+	
+	public static int tempoConversion (double d) {
+	    double beatsPerMillisec = (60000/d);
+	    return (int) beatsPerMillisec;
+	   }
+
 }
