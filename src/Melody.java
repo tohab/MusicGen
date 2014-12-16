66501937235664e16a6f5ac1dd2a174eccb8b9f7
sophiazorek <sophia_zorek@asl.org>
2014-12-16 15:30:26 +0000

    Update Melody.java

--- src/Melody.java
+++ src/Melody.java
@@ -1,24 +1,57 @@
+
+
 import javax.sound.midi.*;
 import javax.swing.Timer;
+//60 = middle c
+//have a method to return the note
 
 public class Melody {
-	// construct a melody based on chords
-	// major = 1 minor = 0
-	public void melody(int key, int tempo, int[] notesInChord)
-			throws InterruptedException, MidiUnavailableException {
-
-		int barlength = tempo * 4;
-		int root = notesInChord[0];
-
-		Synthesizer syn = MidiSystem.getSynthesizer();
-		syn.open();
-		final MidiChannel[] mc = syn.getChannels();
-		Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
-		syn.loadInstrument(instr[109]);
-
-		mc[5].noteOn(root, 100);
-		new Thread().sleep(barlength);
-		mc[5].noteOff(root, 100);
-
-	}
+  // construct a melody based on chords
+  // major = 1 minor = 0
+	private int barlength;
+	private int root;
+	private MidiChannel mc;
+	private int thirdMaj;
+	private int thirdMin;
+  public Melody(int key, int tempo, int quality) throws InterruptedException, MidiUnavailableException {
+  
+       barlength = tempo*4; 
+       root = 72 + key;
+      thirdMaj = 76 + key; 
+      thirdMin = 75 + key;
+       
+       Synthesizer syn = MidiSystem.getSynthesizer();
+       syn.open();
+       mc = syn.getChannels()[5];
+       Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
+       syn.loadInstrument(instr[109]);
+  }
+  
+  public void playRoot () {
+      mc.noteOn(root, 100);
+   
+  
+  }
+  
+  public void stopRoot() {
+	  mc.noteOff(root, 100);
+  }
+  
+  public void playThirdMaj () {
+      mc.noteOn(thirdMaj, 100);
+   
+  
+  }
+  
+  public void stopThirdMaj() {
+	  mc.noteOff(thirdMaj, 100);
+  }
+  public void playThirdMin () {
+	  mc.noteOn(thirdMin, 100);
+  }
+  
+  public void stopThirdMin() {
+	  mc.noteOff(thirdMin, 100);
+  }
+  
 }
